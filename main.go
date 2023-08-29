119834079fb37012c0ff242f3ca36634d4754602
pooja thanekar <pujathanekar@gmail.com>
2023-08-29 12:44:54 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/bradfitz/gomemcache/memcache"
+	"github.com/bradfitz/gomemcache/memcache"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        mc := memcache.New("127.0.0.1:11211") // Replace with your Memcache server address
-        key := "mykey"
-        item, err := mc.Get(key)
-        if err == nil {
-            fmt.Fprintf(w, "Value for key %s: %s", key, item.Value)
-        } else if err == memcache.ErrCacheMiss {
-            fmt.Fprintf(w, "Key %s not found in cache", key)
-        } else {
-            fmt.Fprintf(w, "Error: %v", err)
-        }
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mc := memcache.New("appengine") // Use "appengine" as the server address
+		key := "mykey"
+		item, err := mc.Get(key)
+		if err == nil {
+			fmt.Fprintf(w, "Value for key %s: %s", key, item.Value)
+		} else if err == memcache.ErrCacheMiss {
+			fmt.Fprintf(w, "Key %s not found in cache", key)
+		} else {
+			fmt.Fprintf(w, "Error: %v", err)
+		}
+	})
 
-    http.ListenAndServe(":8086", nil)
+	http.ListenAndServe(":8080", nil)
 }
